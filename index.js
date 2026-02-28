c6a4715cd770249343b2429899fb1b1a999e07be
rafavd <rafarovd@gmail.com>
2026-02-28 01:01:55 -0300

    Refactor puppeteer script with environment variables
    Refactor script to use environment variables for URL and proxy. Implement address input handling and adjust timing for click actions.

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const { connect } = require("puppeteer-real-browser");
-//const { GhostCursor } = require('ghost-cursor');
+
+const MINUTOS = 10;
+const URL = process.env.URL;
+const PROXY = process.env.PROXY || false;
+const ADDRESS = process.env.ADDRESS.split("\n");
+
+const randomIndex = Math.floor(Math.random() * PROXY.length);
 
 const run = async () => {
   const { page, browser } = await connect({
@@ -7,12 +13,7 @@ const run = async () => {
     turnstile: true,
     headless: false,
     // disableXvfb: true,
-    proxy: {
-      host: "104.234.127.10",
-      port: 47568,
-      username: "BHVTZ9TQ",
-      password: "EBD8SU4H",
-    },
+    proxy: PROXY[randomIndex],
     customConfig: {},
     connectOption: {
       defaultViewport: null,
@@ -20,60 +21,41 @@ const run = async () => {
     plugins: [],
   });
 
-  await page.goto("https://thenanobutton.com/u5Xtxg?unit=dollars", {
-    waitUntil: "networkidle2",
-  });
+  try {
+    await page.goto(URL, { waitUntil: "networkidle2" });
 
-  await page.evaluate(() => {
-    document.body.style.zoom = "50%";
-  });
+    await page.evaluate(() => {
+      document.body.style.zoom = "50%";
+    });
 
-  await new Promise((r) => setTimeout(r, 10000));
+    await new Promise((r) => setTimeout(r, 5000));
 
-  await page.waitForSelector("circle");
+    await page.type("#address", ADDRESS[randomIndex]);
+    const value = await page.$eval("#address", (el) => el.value);
+    if (value !== ADDRESS[randomIndex]) {
+      await page.$eval("#address", (el) => (el.value = ""));
+      await page.type("#address", ADDRESS[randomIndex]);
+    }
 
-  let count = 0;
-  while (count < 50) {
-    // await page.realCursor.click("circle")
+    const tempoTotal = MINUTOS * 60 * 1000;
+    const inicio = Date.now();
 
-    // mouse click
-    const circle = await page.$("circle");
-    const boundingBox = await circle.boundingBox();
-    const x = boundingBox.x + boundingBox.width / 2;
-    const y = boundingBox.y + boundingBox.height / 2;
-    await page.mouse.click(x, y);
-    await new Promise((r) => setTimeout(r, 400));
-    count++;
+    while (Date.now() - inicio < tempoTotal) {
+      try {
+        await page.waitForSelector("circle");
+        await page.click("circle");
+      } catch (e) {}
+      await new Promise((r) => setTimeout(r, 400));
+    }
+
+    await page.click("button[type='button'] > span");
+
+    await page.screenshot({ path: "screen.png" });
+  } catch (e) {
+    console.error("erro", e);
+  } finally {
+    await browser.close();
   }
-
-  // await new Promise((r) => setTimeout(r, 5000));
-
-  // let token = null;
-  // let startDate = Date.now();
-  // while (!token && Date.now() - startDate < 30000) {
-  //   token = await page.evaluate(() => {
-  //     try {
-  //       let item = document.querySelector(
-  //         '[name="cf-turnstile-response"]',
-  //       ).value;
-  //       return item && item.length > 20 ? item : null;
-  //     } catch (e) {
-  //       return null;
-  //     }
-  //   });
-  //   await new Promise((r) => setTimeout(r, 1000));
-  // }
-
-  // await new Promise((r) => setTimeout(r, 5000));
-
-
-
-  await page.screenshot({ path: "screen.png" });
-
-  await browser.close();
 };
 
 run();
-
-
-
